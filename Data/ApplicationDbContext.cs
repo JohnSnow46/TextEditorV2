fedaadbd3d56d7548f9032be6922e1d07377aa18
frane <frane.kuna@gmail.com>
2024-08-02 02:28:05 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Models;
+
+namespace TextEditorV2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Doc> Docs { get; set; }
+    }
+}
